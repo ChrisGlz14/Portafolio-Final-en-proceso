c1654b4ae4d4c12d57a4e0d54f64a55a04573bfc
Chrisglz14 <Christiang14@hotmail.com.ar>
2023-05-26 01:23:33 -0300

    Header del portafolio final

---
+++ main.js
@@ -0,0 +1,19 @@
+const btnSwitch = document.querySelector("#switch");
+const btnMobile = document.querySelector(".btnMobile");
+
+
+btnSwitch.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  btnSwitch.classList.toggle("active");
+});
+
+btnMobile.addEventListener("click", () => {
+  let links = document.querySelectorAll(".links");
+  for (const link of links) {
+    if (link.style.display === "none") {
+      link.style.display = "block"; // o cualquier otro valor adecuado para mostrar el elemento
+    } else {
+      link.style.display = "none";
+    }
+  }
+});
