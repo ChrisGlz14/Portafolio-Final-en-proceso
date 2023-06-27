a3da5b3ac2dfa59a00e5e1dd2c2bd5a32bab4c32
Chrisglz14 <Christiang14@hotmail.com.ar>
2023-06-27 05:33:37 -0300

    uniendo partes del portfolio

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 // Nav 
 
-// const btnSwitch = document.querySelector("#switch");
-// const btnMobile = document.querySelector(".btnMobile");
+const btnSwitch = document.querySelector("#switch");
+const btnMobile = document.querySelector(".btnMobile");
 
 
-// btnSwitch.addEventListener("click", () => {
-//   document.body.classList.toggle("dark");
-//   btnSwitch.classList.toggle("active");
-// });
+btnSwitch.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  btnSwitch.classList.toggle("active");
+});
 
-// btnMobile.addEventListener("click", () => {
-//   let links = document.querySelectorAll(".links");
-//   for (const link of links) {
-//     if (link.style.display === "none") {
-//       link.style.display = "block"; // o cualquier otro valor adecuado para mostrar el elemento
-//     } else {
-//       link.style.display = "none";
-//     }
-//   }
-// });
+btnMobile.addEventListener("click", () => {
+  let links = document.querySelectorAll(".links");
+  for (const link of links) {
+    if (link.style.display === "none") {
+      link.style.display = "block"; // o cualquier otro valor adecuado para mostrar el elemento
+    } else {
+      link.style.display = "none";
+    }
+  }
+});
 
 // Principal Presentacion
