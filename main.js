50da27e16569c1695ead4ce6660e04c2f792759d
ChrisGlz14 <christiang14@hotmail.com.ar>
2023-11-28 18:45:03 -0300

    Ingles y español boton

--- main.js
+++ main.js
@@ -90,3 +90,28 @@ function handleSubmit(e) {
 }
 
 
+//INGLES ESPAÑOL SWITCH 
+
+const navElement = document.getElementById("nav");
+
+navElement.addEventListener("click", (e) => {
+  
+    changeLanguage(e.target.dataset.language);
+  
+});
+
+const textsToChange = document.querySelectorAll(`[data-section]`)
+
+const changeLanguage = async language => {
+  const requestJson = await fetch (`./languages/${language}.json`)
+  const texts = await requestJson.json()
+
+  for (const textToChange of textsToChange) {
+    const section = textToChange.dataset.section;
+    const value = textToChange.dataset.value;
+
+    textToChange.innerHTML=texts[section][value]
+  }
+}
+
+
